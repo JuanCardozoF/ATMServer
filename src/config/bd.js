26e71df2f5d84af28b03679e84ea7ff07b294aca
Juan <davidcardozo-2003@hotmail.com>
2024-03-17 12:31:37 -0500

    x3

--- src/config/bd.js
+++ src/config/bd.js
@@ -1,10 +1,22 @@
-const mysql = require('mysql');
+require('dotenv').config();
+const postgres = require('postgres');
 
-module.exports = () =>{
-   return mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "atm"
-    })
-}
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD,PORT2} = process.env;
+
+const sql = postgres({
+    host: PGHOST,
+    database: PGDATABASE,
+    username: PGUSER,
+    password: PGPASSWORD,
+    port: PORT2,
+    ssl: true, 
+});
+
+sql`SELECT NOW()`.then((result) => {
+    console.log('Conexión exitosa a la base de datos.');
+    console.log('Fecha y hora actuales en la base de datos:', result[0].now);
+}).catch((error) => {
+    console.error('Error al conectar a la base de datos:', error);
+});
+
+module.exports = sql;
